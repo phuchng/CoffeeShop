c75be60fad3312810a13c1cb75fdd10ea125b387
ThaiCoder2003 <101780284+ThaiCoder2003@users.noreply.github.com>
2024-12-02 20:11:07 +0700

    Authentication

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
+  res.locals.isAuthenticated = req.isAuthenticated(); // Add authentication status
+  res.locals.user = req.user || null;     
   next();
 })
 // view engine setup
