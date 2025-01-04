f503edb641c25fac8ede511079e0d34f626aad34
phuchng <phuchng0612@gmail.com>
2025-01-04 22:55:33 +0700

    Update manage users and manage products

--- public/js/AProduct.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("sortButton").addEventListener("click", function () {
-    const dropdownMenu = document.getElementById("dropdownMenu");
-    dropdownMenu.classList.toggle("hidden");
-});
